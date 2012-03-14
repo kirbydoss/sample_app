b2ffd06648f5eea181fadc44b617a77a281eeedc
Kirby Doss <kirbydoss@gmail.com>
2012-03-14 12:04:38 -0500

    Getting ready for production

--- Gemfile
+++ Gemfile
@@ -38,6 +38,10 @@ group :test do
   gem 'factory_girl_rails', '1.4.0'
 end
 
+group :production do
+  gem 'pg', '0.12.2'
+end
+
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,6 +25,11 @@ describe "User Pages" do
       it "should not create a user" do
         expect {click_button "Create my account"}.not_to change(User, :count)
       end
+      describe "error messages" do
+        before {click_button "Create my account"}
+        it {should have_selector 'title', text: "Sign Up"}
+        it {should have_content ('error')}
+      end
     end
     
     describe "with valid information" do
@@ -38,6 +43,13 @@ describe "User Pages" do
         expect do
           click_button "Create my account"
         end.to change(User, :count).by(1)
+      end
+      describe "after saving the user" do
+        before {click_button "Create my account"}
+        let(:user) {User.find_by_email('user@example.com')}
+        
+        it {should have_selector 'title', text: user.name}
+        it {should have_selector 'div.alert.alert-success', text: "Welcome"}
       end
     end
   end
