eabe7583037964d0888a7d610c1757597b8cd879
Kirby Doss <kirbydoss@gmail.com>
2012-03-19 20:43:12 -0500

    Refactor signin redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       #Sign the user in and redirect to the user's show page.
       sign_in user
-      redirect_back_or user
+      redirect_back_or root_path
     else
       flash.now[:error] = 'Invalid email/password combination' #not quite right!
       render 'new'
